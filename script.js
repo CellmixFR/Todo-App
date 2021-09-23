735d99ad7efcde7d22884aff5ba1ae5a38cfefbb
Célia Tosic <celiatosic@gmail.com>
2021-09-23 15:35:16 +0200

    save class'complete' in localStorage

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Selectors
+//*********Selectors*************
 let input = document.getElementById('input');
 let ul = document.getElementById('ul');
 
@@ -9,9 +9,8 @@ let completed = document.getElementById("completed");
 let dynamicNumber = document.getElementById("dynamic_number");
 let count = document.querySelector("ul").children.length;
 
-let etat="";
-
-input.addEventListener('keydown', function (e) {  //keypress est maintenant obsolète
+//**************Add Input Content********
+input.addEventListener('keydown', function (e) {  
     if (e.key === 'Enter') {
         e.preventDefault();
         createToDoDiv(input.value);
@@ -19,72 +18,75 @@ input.addEventListener('keydown', function (e) {  //keypress est maintenant obso
 })
 
 function createToDoDiv(InputValue) {
-    //Création de la div
+    //Create div
     let toDoDiv = document.createElement("div");
     toDoDiv.classList.add("todo_div");
     ul.appendChild(toDoDiv);
 
-    //Création de la li
+    //Create li
     let li = document.createElement("li");
     li.classList.add("todo_item");
     toDoDiv.appendChild(li);
-    saveToDoInLocalStorage(input.value);
+    saveToDoInLocalStorage(input.value, li);
 
-    //ajout du bouton "icon_check"
+    //button "check"
     let icon_check = document.createElement("button");
     icon_check.innerHTML = '<img class="icon_check" src="images/icon-check.svg" alt="icon check">';
     icon_check.classList.add("button_icon_check");
     toDoDiv.appendChild(icon_check);
 
-    //Ajout du bouton "cross"
+    //button "cross"
     let icon_cross = document.createElement("button");
     icon_cross.innerHTML = '<img class="icon_cross" src="images/icon-cross.svg" alt="icon cross">';
     icon_cross.classList.add("button_icon_cross");
     toDoDiv.appendChild(icon_cross);
 
-    //On veut que le texte du li soit égal au texte de l'input
     li.innerHTML = InputValue;
-    input.value = ""; //On réinitialise l'input
+    input.value = ""; //reset input
 
+    //Functions calls 
     completeToDo(icon_check, li);
-    deleteToDo(toDoDiv, icon_cross, ul);
+    deleteToDo(toDoDiv, icon_cross, ul, li.innerHTML);
     showAllToDo(toDoDiv);
     showActiveToDo(toDoDiv, li);
     showCompletedToDo(toDoDiv, li);
-    deleteCompletedToDo(toDoDiv, li, ul);
+    deleteCompletedToDo(toDoDiv, li, ul, li.innerHTML);
     updateCountItemsLeft();
-};
+}
+
+//*************Functions**************/
 
 function completeToDo(Icon_check, Li) {
     Icon_check.addEventListener('click', completedToDo);
     function completedToDo() {
-        Li.classList.toggle("complete");       
+        todo = Li.innerHTML;
+        localStorage.setItem(todo, !Li.classList.contains('complete'));
+        Li.classList.toggle("complete");
         Icon_check.classList.toggle("complete");
+
         //Update Count if the icon_check is checked
         if (Li.classList.contains("complete")) {
             dynamicNumber = document.getElementById("dynamic_number");
-            dynamicNumber.innerHTML--;          
-            // saveStatusInLocalStorage(etat);
+            dynamicNumber.innerHTML--;
         } else {
             dynamicNumber.innerHTML++;
-            // removeLocalStatus(etat);
-            // saveStatusInLocalStorage(etat);
-        }   
+        }
     }
 }
-function deleteToDo(ToDoDiv, Icon_cross, Ul) {
+
+function deleteToDo(ToDoDiv, Icon_cross, Ul, key) {
     Icon_cross.addEventListener('click', () => {
         Ul.removeChild(ToDoDiv);
-        removeLocalTodos(ToDoDiv);
+        removeLocalTodos(key);
         updateCountItemsLeft();
-    });
+    })
 }
 
 function showAllToDo(ToDoDiv) {
     all.addEventListener("click", () => {
         ToDoDiv.style.display = "flex";
         changeColorofLinks(all, active, completed);
-    });
+    })
 }
 
 function showActiveToDo(ToDoDiv, Li) {
@@ -96,7 +98,7 @@ function showActiveToDo(ToDoDiv, Li) {
         } else {
             ToDoDiv.style.display = "none";
         }
-    });
+    })
 }
 
 function showCompletedToDo(ToDoDiv, Li) {
@@ -107,18 +109,20 @@ function showCompletedToDo(ToDoDiv, Li) {
         } else {
             ToDoDiv.style.display = "none";
         }
-    });
+    })
 }
 
-function deleteCompletedToDo(ToDoDiv, Li, Ul) {
+function deleteCompletedToDo(ToDoDiv, Li, Ul, key) {
     let clearCompleted = document.getElementById("clear_completed");
     clearCompleted.addEventListener("click", () => {
         if (Li.classList.contains('complete')) {
             Ul.removeChild(ToDoDiv);
+            removeLocalTodos(key)
         } else {
             ToDoDiv.style.display = "flex";
         }
-    });
+        
+    })
 }
 
 function updateCountItemsLeft() {
@@ -137,125 +141,71 @@ function changeColorofLinks(ColoredLink, NormalLink1, NormalLink2) {
 //************************************/
 
 // Save todos in local storage 
-function saveToDoInLocalStorage(todo) {
-    let todos;
-    if (localStorage.getItem("todos") === null) {  //localStorage.getItem("clé") --> renvoie les valeurs correspondantes à la clé passée en paramètre. Ici, s'il n'y a pas de valeur, on crée un tableau vide 
-        todos = [];
-    } else {
-        todos = JSON.parse(localStorage.getItem("todos")); //JSON.parse() transforme une chaîne JSON en un objet JavaScript.Si les valuers existent, alors on les stocke dans la variable todos
-    }
-    todos.push(todo);
-    localStorage.setItem("todos", JSON.stringify(todos)); //La méthode setItem(), lorsque lui sont passées le duo clé-valeur, les ajoute à l'emplacement de stockage, sinon elle met à jour la valeur si la clé existe déjà. JSON.stringify() transforme un objet JavaScript en une chaîne JSON.
+function saveToDoInLocalStorage(todo, Li) {
+    localStorage.setItem(todo, Li.classList.contains('complete')); //La méthode setItem(), lorsque lui sont passées le duo clé-valeur, les ajoute à l'emplacement de stockage, sinon elle met à jour la valeur si la clé existe déjà. JSON.stringify() transforme un objet JavaScript en une chaîne JSON.
 }
 
 document.addEventListener("DOMContentLoaded", getToDo); //Dès que la page se charge, on appelle la fonction
 
-function getToDo() {
-    let todos;
-    if (localStorage.getItem("todos") === null) {
-        todos = [];
-    } else {
-        todos = JSON.parse(localStorage.getItem("todos"));
+    function getToDo() {
+        // Pour chaque clé :
+        //   Récupérer la clé (qui correspond à une todo)
+        //   Récupérér son été (true ou false qui correspond à complété ou non)
+        //   Afficher le todo dans la div et le li et défini la classe complete si il vaut true
+
+        
+        for (let i = 0; i < localStorage.length; i++) {
+            let key = localStorage.key(i);
+            
+            //Create div
+            let toDoDiv = document.createElement("div");
+            toDoDiv.classList.add("todo_div");
+            ul.appendChild(toDoDiv);
+
+            //Create li
+            let li = document.createElement("li");
+            li.classList.add("todo_item");
+            toDoDiv.appendChild(li);
+            
+
+            //Button "check"
+            let icon_check = document.createElement("button");
+            icon_check.innerHTML = '<img class="icon_check" src="images/icon-check.svg" alt="icon check">';
+            icon_check.classList.add("button_icon_check");
+            toDoDiv.appendChild(icon_check);
+
+
+            //Button "cross"
+            let icon_cross = document.createElement("button");
+            icon_cross.innerHTML = '<img class="icon_cross" src="images/icon-cross.svg" alt="icon cross">';
+            icon_cross.classList.add("button_icon_cross");
+            toDoDiv.appendChild(icon_cross);
+
+            li.innerHTML = key;
+            input.value = ""; //reset input
+
+            if(localStorage.getItem(key)== "true" ){
+                li.classList.add("complete");
+                icon_check.classList.add("complete");
+                console.log(localStorage.getItem(key));
+            } else if(localStorage.getItem(key)== "false" ){
+                console.log(localStorage.getItem(key));
+            }
+        
+            completeToDo(icon_check, li);   
+            deleteToDo(toDoDiv, icon_cross, ul, li.innerHTML);
+            showAllToDo(toDoDiv);
+            showActiveToDo(toDoDiv, li);
+            showCompletedToDo(toDoDiv, li);
+            deleteCompletedToDo(toDoDiv, li, ul, li.innerHTML);
+            updateCountItemsLeft();
+        }
     }
-
-    todos.forEach(function (todo) {
-        //Création de la div
-        let toDoDiv = document.createElement("div");
-        toDoDiv.classList.add("todo_div");
-        ul.appendChild(toDoDiv);
-
-        //Création de la li
-        let li = document.createElement("li");
-        li.classList.add("todo_item");
-        toDoDiv.appendChild(li);
-
-        //ajout du bouton "icon_check"
-        let icon_check = document.createElement("button");
-        icon_check.innerHTML = '<img class="icon_check" src="images/icon-check.svg" alt="icon check">';
-        icon_check.classList.add("button_icon_check");
-        toDoDiv.appendChild(icon_check);
-
-        //Ajout du bouton "cross"
-        let icon_cross = document.createElement("button");
-        icon_cross.innerHTML = '<img class="icon_cross" src="images/icon-cross.svg" alt="icon cross">';
-        icon_cross.classList.add("button_icon_cross");
-        toDoDiv.appendChild(icon_cross);
-
-        //On veut que le texte du li soit égal au texte de l'input
-        li.innerHTML = todo;
-        input.value = ""; //On réinitialise l'input
-
-        completeToDo(icon_check, li);
-        deleteToDo(toDoDiv, icon_cross, ul);
-        showAllToDo(toDoDiv);
-        showActiveToDo(toDoDiv, li);
-        showCompletedToDo(toDoDiv, li);
-        deleteCompletedToDo(toDoDiv, li, ul);
-        updateCountItemsLeft();
-
-    })
-
-}
 
 //Delete todos in local storage
-function removeLocalTodos(todo) {
-    let todos;
-    if (localStorage.getItem("todos") === null) {
-        todos = [];
-    } else {
-        todos = JSON.parse(localStorage.getItem("todos"));
-    }
-    const todoIndex = todo.children[0].innerText;
-    console.log(todo.children);
-    todos.splice(todos.indexOf(todoIndex), 1);  //La méthode splice() modifie le contenu d'un tableau en retirant des éléments et/ou en ajoutant de nouveaux éléments à même le tableau.On peut ainsi vider ou remplacer une partie d'un tableau. Prend en paramètre l'index à partir du quel il faut supprimer l'élément puis prend le nombre d'éléments à supprimer  
-    localStorage.setItem("todos", JSON.stringify(todos));
+function removeLocalTodos(key) {
+  localStorage.removeItem(key);
 }
-
-//**********************Pareil mais pour le statu completed/not completed*******************/
-
-// function saveStatusInLocalStorage(state) {
-//     let status;
-//     if ((localStorage.getItem("status") === null)) {
-//         status = [];
-//     } else {
-//         status = JSON.parse(localStorage.getItem("status"));
-//     }
-//     status.push(state);
-//     localStorage.setItem("status", JSON.stringify(status));
-// }
-
-
-// document.addEventListener("DOMContentLoaded", getStatus);
-
-// function getStatus() {
-//     let status;
-//     if ((localStorage.getItem("status") === null)) {
-//         status = [];
-//     } else {
-//         status = JSON.parse(localStorage.getItem("status"));
-//     }
-//     status.forEach(function(state){
-
-       
-//     })}
-
-
-// //Delete status in local storage
-// function removeLocalStatus(state) {
-//     let status;
-//     if (localStorage.getItem("status") === null) {
-//         status = [];
-//     } else {
-//         status = JSON.parse(localStorage.getItem("status"));
-//     }
-//     const statuIndex = state[1].innerText;
-//     status.splice(status.indexOf(statuIndex), 1);  //La méthode splice() modifie le contenu d'un tableau en retirant des éléments et/ou en ajoutant de nouveaux éléments à même le tableau.On peut ainsi vider ou remplacer une partie d'un tableau. Prend en paramètre l'index à partir du quel il faut supprimer l'élément puis prend le nombre d'éléments à supprimer  
-//     localStorage.setItem("status", JSON.stringify(status));
-//  }
-
-
-/***********************************************************************/
-
 
 //*****************Dark Mode********************
 
